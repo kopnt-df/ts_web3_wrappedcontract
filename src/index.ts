a9ae88cfabb3e56263e698e89e4d47c6d4acd7d9
Kristof <kristof.kovacs@telenav.com>
2021-05-12 21:55:45 +0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './wrappedContract/wrappedContract'
 
 export { Eth } from 'web3-eth'
-export { Contract, Options } from 'web3-eth-contract';
+export { Contract, Options, ContractSendMethod } from 'web3-eth-contract';
 export { AbiItem } from 'web3-utils';
